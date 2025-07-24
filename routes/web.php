a14e64ac0e4310b8fb425bb2c57666826da2a986
linporu <qazwsx123456guy@gmail.com>
2025-07-24 14:20:56 +0800

    refactor(routes): restructure route definitions for EventController - Changed separate route definitions to use a controller group for better organization - Simplified syntax for defining routes associated with EventController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [EventController::class, 'index'])->name('events.index');
-
-Route::post('/', [EventController::class, 'store'])->name('events.store');
+Route::controller(EventController::class)->group(function () {
+    Route::get('/', 'index')->name('events.index');
+    Route::post('/', 'store')->name('events.store');
+});
